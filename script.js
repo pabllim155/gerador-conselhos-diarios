ce1bf417aa769e6fbcdbb05f69fbf812564e5eba
pablo rodrigo cardoso de oliveira <pablo762rc@gmail.com>
2025-09-24 21:25:40 -0300

    criaçao de upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+const conselhos =[
+    "acredite em voce mesmo .","o primeiro passo é o mais importante.",
+    "a persistencia realiza o impossivel .","aprenda algo novo hoje.",
+    "seja gentil com todos que encontra ."
+]
+console.log("arquivode script carregado.array de conselhos pronto.");
